d4ed9c2458df226c7626b9d6603ce897be15ff6d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-05-11 20:26:43 -0400

    add more markdown fixtures

--- test/parse.test.js
+++ test/parse.test.js
@@ -9,8 +9,24 @@ const example1 = readFileSync(
   'utf8',
 )
 
+const example2 = readFileSync(
+  join(__dirname, 'fixtures', 'example2.md'),
+  'utf8',
+)
+
 test('example1', (t) => {
   t.plan(1)
   const parsed = parse(example1)
   t.snapshot(parsed)
+})
+
+test('example2', (t) => {
+  t.plan(0)
+  const parsed = parse(example2)
+  // TODO
+  // we want to make sure the anchor links
+  // were parsed and only the text remains
+  // and the links themselves were removed
+  // t.snapshot(parsed)
+  console.log(parsed)
 })
